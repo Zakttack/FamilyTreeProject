094dd8cd96988df83df465ea2509574da43f6793
Zakttack <zak.merrigan@ndsu.edu>
2023-09-12 11:47:15 -0500

    Wrote method for resources

--- FamilyTreeLibrary/FamilyTreeUtils.cs
+++ FamilyTreeLibrary/FamilyTreeUtils.cs
@@ -3,66 +3,66 @@ using Newtonsoft.Json;
 namespace FamilyTreeLibrary
 {
     public class FamilyTreeUtils
-{
-    public static DateTime DefaultDate
     {
-        get
+        public static DateTime DefaultDate
         {
-            return new();
-        }
-    }
-
-    internal static IComparer<Family> Comparer
-    {
-        get
-        {
-            return new PersonComparer();
-        }
-    }
-
-    public static int CompareDates(DateTime a, DateTime b)
-    {
-        int yearDiff = a.Year - b.Year;
-        int monthDiff = a.Month - b.Month;
-        int dayDiff = a.Day - b.Day;
-        if (yearDiff == 0)
-        {
-            if (monthDiff == 0)
+            get
             {
-                return dayDiff;
+                return new();
             }
-            return monthDiff;
         }
-        return yearDiff;
-    }
 
-    public static string GetFileNameFromResources(string fileNameWithExtension)
-    {
-        return GetFileNameFromResources(Directory.GetCurrentDirectory(), fileNameWithExtension);
-    }
-
-    public static string WriteDate(DateTime a)
-    {
-        return $"{a.Month}/{a.Day}/{a.Year}";
-    }
-
-    private static string GetFileNameFromResources(string currentPath, string fileNameWithExtension)
-    {
-        string[] parts = currentPath.Split('\\');
-        string current = parts[^1];
-        if (current != "FamilyTreeProject")
+        internal static IComparer<Family> Comparer
         {
-            return GetFileNameFromResources(currentPath[..(currentPath.Length - current.Length - 1)], fileNameWithExtension);
+            get
+            {
+                return new PersonComparer();
+            }
         }
-        return $"{currentPath}\\Resources\\{fileNameWithExtension}";
-    }
 
-    private class PersonComparer : IComparer<Family>
-    {
-        public int Compare(Family a, Family b)
+        public static int CompareDates(DateTime a, DateTime b)
         {
-            return CompareDates(a.Couple[0].BirthDate, b.Couple[0].BirthDate);
+            int yearDiff = a.Year - b.Year;
+            int monthDiff = a.Month - b.Month;
+            int dayDiff = a.Day - b.Day;
+            if (yearDiff == 0)
+            {
+                if (monthDiff == 0)
+                {
+                    return dayDiff;
+                }
+                return monthDiff;
+            }
+            return yearDiff;
+        }
+
+        public static string GetFileNameFromResources(string fileNameWithExtension)
+        {
+            return GetFileNameFromResources(Directory.GetCurrentDirectory(), fileNameWithExtension);
+        }
+
+        public static string WriteDate(DateTime a)
+        {
+            return $"{a.Month}/{a.Day}/{a.Year}";
+        }
+
+        private static string GetFileNameFromResources(string currentPath, string fileNameWithExtension)
+        {
+            string[] parts = currentPath.Split('\\');
+            string current = parts[^1];
+            if (current != "FamilyTreeProject")
+            {
+                return GetFileNameFromResources(currentPath[..(currentPath.Length - current.Length - 1)], fileNameWithExtension);
+            }
+            return $"{currentPath}\\Resources\\{fileNameWithExtension}";
+        }
+
+        private class PersonComparer : IComparer<Family>
+        {
+            public int Compare(Family a, Family b)
+            {
+                return CompareDates(a.Couple[0].BirthDate, b.Couple[0].BirthDate);
+            }
         }
     }
-}
 }
