c505f5c7f61c96335fa6ff6f20327510f52ada11
Zakttack <zak.merrigan@ndsu.edu>
2023-10-18 15:20:36 -0500

    Realized that Danielle Lee Krahler isn't being printed out

--- FamilyTreeScratch/Program.cs
+++ FamilyTreeScratch/Program.cs
@@ -3,12 +3,16 @@ using FamilyTreeLibrary.OrderingType;
 using FamilyTreeLibrary.PDF;
 
 Queue<string> textLines = PdfUtils.GetPDFLinesAsQueue(26, @"C:\Users\zakme\Documents\FamilyTreeProject\Resources\PfingstenBook2023.pdf");
-IReadOnlyDictionary<AbstractOrderingType[],Queue<KeyValuePair<int,Family>>> nodesWithoutChildren = PdfUtils.ParseAsFamilyNodes(textLines);
-foreach (KeyValuePair<AbstractOrderingType[],Queue<KeyValuePair<int,Family>>> node in nodesWithoutChildren)
+foreach (string line in textLines)
 {
-    foreach (KeyValuePair<int,Family> familyPairs in node.Value)
-    {
-        Console.WriteLine(familyPairs.Value);
-        Console.WriteLine();
-    }
+    Console.WriteLine(line);
 }
+// IReadOnlyDictionary<AbstractOrderingType[],Queue<KeyValuePair<int,Family>>> nodesWithoutChildren = PdfUtils.ParseAsFamilyNodes(textLines);
+// foreach (KeyValuePair<AbstractOrderingType[],Queue<KeyValuePair<int,Family>>> node in nodesWithoutChildren)
+// {
+//     foreach (KeyValuePair<int,Family> familyPairs in node.Value)
+//     {
+//         Console.WriteLine(familyPairs.Value);
+//         Console.WriteLine();
+//     }
+// }
