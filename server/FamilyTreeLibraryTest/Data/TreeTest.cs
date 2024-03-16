352e1c4573bdf6c93905c880a42664cab2263666
Zakttack <zak.merrigan@ndsu.edu>
2024-03-16 13:47:02 -0500

    Making a temporary commit to move the project directory relative to file explorer.

--- server/FamilyTreeLibraryTest/Data/TreeTest.cs
+++ server/FamilyTreeLibraryTest/Data/TreeTest.cs
@@ -23,5 +23,35 @@ namespace FamilyTreeLibraryTest.Data
             Family child = new(new Person("Cade Alan Merrigan", new FamilyTreeDate("29 Oct 2000"), FamilyTreeDate.DefaultDate), null, FamilyTreeDate.DefaultDate);
             Assert.That(familyTree.Contains(parent, child), Is.True);
         }
+
+        [Test]
+        public void TestGetEnumerator()
+        {
+            Exception problem = null;
+            try
+            {
+                foreach (Family family in familyTree)
+                {
+                    FamilyTreeUtils.LogMessage(LoggingLevels.Debug, family is null ? "unknown" : family.ToString());
+                }
+            }
+            catch (Exception ex)
+            {
+                problem = ex;
+                FamilyTreeUtils.LogMessage(LoggingLevels.Fatal,$"{ex.GetType().Name}: {ex.Message}\n{ex.StackTrace}");
+            }
+            finally
+            {
+                Assert.That(problem, Is.Null);
+            }
+        }
+
+        [Test]
+        public void TestGetParent1()
+        {
+            Family child = new("Cade Alan Merrian (29 Oct 2000 - Present)");
+            Family expectedParent = new("[Brent Alan Merrigan (24 Dec 1970 - Present)]-[Jolene Joyce Rants (12 Jun 1968 - Present)]: 30 Aug 1997");
+            Assert.That(familyTree.GetParent(child), Is.EqualTo(expectedParent));
+        }
     }
 }
