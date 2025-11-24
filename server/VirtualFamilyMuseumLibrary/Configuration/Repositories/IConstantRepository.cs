604ae78dc1de1f6961d075a36e0b1429840b5f06
Zak Ray Merrigan <zakmerrigan@gmail.com>
2025-11-24 08:18:48 -0600

    Need to delete before resetting

--- server/VirtualFamilyMuseumLibrary/Configuration/Repositories/IConstantRepository.cs
+++
@@ -1,9 +0,0 @@
-namespace VirtualFamilyMuseumLibrary.Configuration.Repositories
-{
-    public interface IConstantRepository
-    {
-        public C? BindSection<C>(string key);
-        public string? GetValue(string key);
-        
-    }
-}
