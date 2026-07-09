eeac1367ef7951e3b47937ecdb80bd1e8d5f5f85
Zak Ray Merrigan <zakmerrigan@gmail.com>
2026-07-09 15:50:13 -0500

    Restarting Azure Stuff

--- server/VirtualFamilyMuseumLibrary/ConstantStore/Repositories/INonSensitiveConstantRepository.cs
+++
@@ -1,9 +0,0 @@
-namespace VirtualFamilyMuseumLibrary.ConstantStore.Repositories
-{
-    public interface INonSensitiveConstantRepository
-    {
-        public C? BindSection<C>(string key);
-        public string? GetValue(string key);
-        
-    }
-}
