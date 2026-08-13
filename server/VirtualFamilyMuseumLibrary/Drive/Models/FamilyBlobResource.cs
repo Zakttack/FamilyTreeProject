0e03e0c7ab866d2ac2a63c7e9bf41cfa4e4d3a8b
Zak Ray Merrigan <zakmerrigan@gmail.com>
2026-08-13 13:41:49 -0500

    Sketched Family Drive Repository

---
+++ server/VirtualFamilyMuseumLibrary/Drive/Models/FamilyBlobResource.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace VirtualFamilyMuseumLibrary.Drive.Models
+{
+    public class FamilyBlobResource
+    {
+        public required string BlobName
+        {
+            get;
+            init;
+        }
+
+        public required string BlobUrl
+        {
+            get;
+            init;
+        }
+
+        public required Stream Content
+        {
+            get;
+            init;
+        }
+
+        public required string ContentType
+        {
+            get;
+            init;
+        }
+    }
+}
---
+++ server/VirtualFamilyMuseumLibrary/Drive/Repository/IFamilyDriveRepository.cs
@@ -0,0 +1,11 @@
+using VirtualFamilyMuseumLibrary.Drive.Models;
+
+namespace VirtualFamilyMuseumLibrary.Drive.Repository
+{
+    public interface IFamilyDriveRepository
+    {
+        public Task<FamilyBlobResource> DeleteAsync(string blobName);
+        public Task<FamilyBlobResource> GetAsync(string blobName);
+        public Task<FamilyBlobResource> UploadAsync(string blobName, Stream content, string contentType);
+    }
+}
