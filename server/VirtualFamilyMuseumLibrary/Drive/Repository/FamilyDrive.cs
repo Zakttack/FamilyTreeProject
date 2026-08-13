b3a300d4800c2789ce71a7c967d78084ecdded04
Zak Ray Merrigan <zakmerrigan@gmail.com>
2026-08-13 13:51:28 -0500

    Created Blob Storage Dependency for Family Drive Repository

---
+++ server/VirtualFamilyMuseumLibrary/Drive/Repository/FamilyDrive.cs
@@ -0,0 +1,10 @@
+using Azure.Storage.Blobs;
+using VirtualFamilyMuseumLibrary.Drive.Models;
+
+namespace VirtualFamilyMuseumLibrary.Drive.Repository
+{
+    public class FamilyDrive
+    {
+        
+    }
+}
